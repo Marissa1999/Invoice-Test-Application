3f8f0a2c907c0d805658d2ae0a7556fec7abb5a5
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 13:58:08 -0500

    Initial Commit.

---
+++ Problem01/Invoice.cs
@@ -0,0 +1,118 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 1 - Problem 1: InvoiceTest app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01
+{
+
+	class Invoice
+	{
+
+        //Initialize the instance variables of the Invoice class
+        private string partNumber;
+        private string partDescription;
+        private int quantity;
+		private decimal pricePerItem;
+
+
+        //Main constructor for the Invoice class that takes all inputted values in the parameter
+		public Invoice(string partNumber, string partDescription, int quantity, decimal pricePerItem)
+		{
+			PartNumber = partNumber;
+			PartDescription = partDescription;
+		    Quantity = quantity;
+			PricePerItem = pricePerItem;
+		}
+
+
+        //Implicit property for the part number
+        public string PartNumber
+        {
+            get; set;
+        }
+
+
+        //Implicit property for the part description
+        public string PartDescription
+		{
+            get; set;
+        }
+
+
+
+        //Property for the quantity
+        public int Quantity
+		{
+
+			get
+			{
+                //Return the quantity
+				return this.quantity;
+			}
+
+			set
+			{
+                //If the value is less than 0, set the quantity to 0
+				if (value < 0)
+				{
+                    this.quantity = 0;
+				}
+
+                //Or else, the quantity is set to the value
+                else
+                {
+                    this.quantity = value;
+                }
+
+			}
+
+		}
+
+
+        //Property for the price per item
+		public decimal PricePerItem
+		{
+
+			get
+			{
+                //Return the pricer per item
+                return this.pricePerItem;
+			}
+
+			set
+			{
+                //If the value is less than 0.0, set the price per item to 0.0
+                if (value < 0.0m)
+				{
+					this.pricePerItem = 0.0m;
+				}
+
+                //Or else, the price per item is set to the value
+                else
+                {
+                    this.pricePerItem = value;
+                }
+
+			}
+
+		}
+
+    
+        //Returns the invoice amount based on the quantity multiplied by the price per item
+        public decimal GetInvoiceAmount()
+		{
+			return Quantity * PricePerItem;
+		}
+
+
+
+	}
+
+
+}
---
+++ Problem01/InvoiceTest.cs
@@ -0,0 +1,66 @@
+﻿//ID: 1775227
+//Name: Marissa Goncalves
+//Assignment 1 - Problem 1: InvoiceTest app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01
+{
+
+	class InvoiceTest
+	{
+
+			static void Main(string[] args)
+			{
+
+                //Prompt the user to type the part number
+				Console.Write("Please enter the part number: ");
+				string number = Console.ReadLine();
+
+                //Prompt the user to type the part description
+                Console.Write("Please enter the part description: ");
+				string description = Console.ReadLine();
+
+                //Prompt the user to type the quantity
+                Console.Write("Please enter the quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                //Prompt the user to type the price per item
+                Console.Write("Please enter the price per item: ");
+				decimal price = decimal.Parse(Console.ReadLine());
+
+
+                //Skip a line
+			    Console.WriteLine();
+			
+
+                //Create a new object with the inputted values
+		     	Invoice invoiceResults = new Invoice(number, description, quantity, price);
+			
+
+                //Using string interpolation, display the inputted results in an organized table
+		     	Console.WriteLine($"----------------------------------------------------");
+		    	Console.WriteLine($"|                    INVOICE                       |");
+			    Console.WriteLine($"----------------------------------------------------");
+			    Console.WriteLine($"| Part Number      | {invoiceResults.PartNumber,-30}|");
+			    Console.WriteLine($"----------------------------------------------------");
+			    Console.WriteLine($"| Part Description | {invoiceResults.PartDescription,-30}|");
+			    Console.WriteLine($"----------------------------------------------------");
+			    Console.WriteLine($"| Quantity         | {invoiceResults.Quantity,-30}|");
+			    Console.WriteLine($"----------------------------------------------------");
+			    Console.WriteLine($"| Price Per Item   | {invoiceResults.PricePerItem,-30:C}|");
+			    Console.WriteLine($"----------------------------------------------------");
+			    Console.WriteLine($"| Total Amount     | {invoiceResults.GetInvoiceAmount(),-30:C}|");
+			    Console.WriteLine($"----------------------------------------------------");
+                Console.WriteLine();
+
+		    }
+
+    }
+
+
+}
